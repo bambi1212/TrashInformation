58b513da362b4d2b540897658ae2ed9269b0ec30
jhonu <yehonatan.reznik@gmail.com>
2024-03-19 13:32:10 +0200

    login and register and logout work

--- app/src/main/java/com/example/trashinformation/MlActivity.java
+++ app/src/main/java/com/example/trashinformation/MlActivity.java
@@ -1,6 +1,9 @@
 package com.example.trashinformation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MlActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ml);
 
     }
-}
+    public void logout(View v) {
+
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(MlActivity.this, LoginActivity.class));
+            finish(); //so he cant press back button and come to here from login
+        }
+    }
--- app/src/main/java/com/example/trashinformation/RegisterActivity.java
+++ app/src/main/java/com/example/trashinformation/RegisterActivity.java
@@ -3,22 +3,50 @@ package com.example.trashinformation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    protected FirebaseAuth mAuth; //he is protected so i can use in register
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        mAuth= FirebaseAuth.getInstance();
 
-        }
+    }
+
+    public void registerAccount(View v) {
+        EditText emailText = findViewById(R.id.edittext_email_login);
+        EditText passwordText = findViewById(R.id. edittext_password_reg);
+
+        mAuth.createUserWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) { //if register work move to welcome
+                            //notificationStart(); //ty for saving Eart notification
+                            startActivity(new Intent(RegisterActivity.this, MlActivity.class));
+                        } else
+                        {
+                            Toast.makeText(RegisterActivity.this, "register failed",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
     public void moveToLoginActivity(View v){
         startActivity(new Intent(this, LoginActivity.class));
 
